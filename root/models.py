fcd367cdd7a0760a9b640d47e6bd00c0c533a0ca
Maruf Khan <whoismaruf@outlook.com>
2020-10-07 10:19:55 +0600

    fixing

--- root/models.py
+++ root/models.py
@@ -2,4 +2,4 @@ from root import db
 
 class Todo(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    task = db.Column(db.String(120))
+    task = db.Column(db.String(120), unique=True)
